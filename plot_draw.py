0649aa2f75f259044d61c5d11affff7286b1bb30
Aviv Palivoda <palaviv@gmail.com>
2018-05-06 18:36:47 +0300

    Remove axis

--- plot_draw.py
+++ plot_draw.py
@@ -10,10 +10,12 @@ for ix, group in enumerate(groups):
     path = os.path.join(PATH, group)
     for i in os.listdir(path):
         plt.subplot(len(groups), 10, ix * 10 + int(i) + 1)
-        if i == '5':
-            plt.title(group)
+        if i == '0':
+            plt.ylabel(group)
         X = np.loadtxt(os.path.join(path, i), dtype=int)
-        plt.imshow(X, cmap=plt.get_cmap('gray'))
+        fig = plt.imshow(X, cmap=plt.get_cmap('gray'))
+        fig.axes.get_xaxis().set_ticks([])
+        fig.axes.get_yaxis().set_ticks([])
 
 # show the plot
 plt.show()
