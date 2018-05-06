44ac8d048911f6c8cbd4cc187307c9c4c4103cb6
Aviv Palivoda <palaviv@gmail.com>
2018-05-06 09:45:01 +0300

    Add numbers to plot titles

--- plot.py
+++ plot.py
@@ -4,12 +4,16 @@ import matplotlib.pyplot as plt
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 # plot 4 images as gray scale
 plt.subplot(221)
+plt.title(y_train[0])
 plt.imshow(X_train[0], cmap=plt.get_cmap('gray'))
 plt.subplot(222)
+plt.title(y_train[1])
 plt.imshow(X_train[1], cmap=plt.get_cmap('gray'))
 plt.subplot(223)
+plt.title(y_train[2])
 plt.imshow(X_train[2], cmap=plt.get_cmap('gray'))
 plt.subplot(224)
+plt.title(y_train[3])
 plt.imshow(X_train[3], cmap=plt.get_cmap('gray'))
 # show the plot
 plt.show()
